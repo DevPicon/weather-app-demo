25c9b502b5a4b30625d0519e89ddf1da290f937a
Armando <picondev@gmail.com>
2020-07-12 17:03:38 -0400

    add landing design

---
+++ app/src/main/java/pe/devpicon/android/myweatherapp/presentation/LandingActivity.kt
@@ -0,0 +1,12 @@
+package pe.devpicon.android.myweatherapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pe.devpicon.android.myweatherapp.R
+
+class LandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
